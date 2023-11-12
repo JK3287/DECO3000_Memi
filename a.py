624be36b39aa2004baf1f8d072a77a86c50232a8
JK3287 <jkwo3287@uni.sydney.edu.au>
2023-11-12 22:50:41 +1100

    .

--- a.py
+++ a.py
@@ -327,7 +327,7 @@ if selected == "FRIENDS":
                 friend = friends[i]
 
                 # You can use a custom profile picture here
-                col.image("https://i.ibb.co/6nCCgkt/icon-user.png", width=100)
+                col.image("https://i.ibb.co/6nCCgkt/icon-user.png", width=100, use_container_width=True)
                 col.markdown(
                     f"<p style='font-size: 24px; font-family: Montserrat;'><strong>{friend['Name']}</strong></p>",
                     unsafe_allow_html=True
